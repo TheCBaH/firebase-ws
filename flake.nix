323beaa2edefc830a0962ec3e90f77af77a67fe8
thecbah <thecbah@gmail.com>
2025-05-01 03:43:27 +0000

    Initial project setup with Nix and basic configuration

---
+++ flake.nix
@@ -0,0 +1,20 @@
+nix
+{
+  description = "OCaml development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in
+      pkgs.mkShell {
+        packages = with pkgs; [
+          ocaml
+        ];
+    };
+  }; 
+}
