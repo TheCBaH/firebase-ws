4163a6aaacd409f087cf34664b55bd1362c95a02
thecbah <thecbah@gmail.com>
2025-05-01 04:39:07 +0000

    Add new files

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,19 +5,24 @@
   channel = "stable-24.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.ocaml
+    pkgs.ocamlPackages.ocaml
+    pkgs.ocamlPackages.dune_3
+    pkgs.ocamlPackages.utop
+    pkgs.ocamlPackages.ocaml-lsp
+    pkgs.ocamlPackages.ocamlformat
+    pkgs.gcc
     # pkgs.python311
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
   ];
   # Sets environment variables in the workspace
-  env = {};
+  env = { };
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      # "vscodevim.vim"
-    ];
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id" or use vscode market place
+    extensions = [      
+      "ocamllabs.ocaml-platform"
+      "vscodevim.vim"];
     # Enable previews
     previews = {
       enable = true;
---
+++ test.ml
@@ -0,0 +1,5 @@
+let () = print_endline "Hello, World!"
+
+let _ = 1
+
+let _ = Printf.sprintf "Hello world"
